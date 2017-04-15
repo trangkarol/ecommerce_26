17f4d6226e5df0505dee6f3b6b9c5a97f08bb553
Ho Thi Thien Trang <thientrang2808@gmail.com>
2017-04-15 11:02:52 +0700

    view admin, lang

--- config/setting.php
+++ config/setting.php
@@ -1,9 +1,27 @@
 <?php
 
 return [
+    'minutes' => 60,
+    'defaul_select' => [
+        '0' => '',
+    ],
+
+    'admin' => [
+        'paginate' => 15,
+    ],
+
+    'path' => [
+        'file' => base_path() . '/public/Upload/',
+        'show' => '/Upload',
+    ],
 
     'images' => [
         'avatar' => 'avatar.jpg',
         'product' => 'product.jpg',
+    ],
+
+    'mutil-level' => [
+        'one' => 0,
+        'two' => 1,
     ],
 ];
---
+++ resources/lang/en/common.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    // title
+    'title-system' => 'Ecommerce',
+    // label
+    'lbl-result-search' => 'Result search',
+    'lbl-welcome' => 'Welcome, ',
+    'lbl-genaral' => 'Genaral',
+    'lbl-stt' => 'No.',
+    'file' => [
+        'import' => 'Import File',
+        'export' => 'Export File',
+    ],
+
+    'button' => [
+        'reset' => 'Reset',
+        'edit' => 'Edit',
+        'delete' => 'Delete',
+        'insert' => 'Insert',
+    ],
+
+    'made_in' => [
+        'chosse' => '--Made in--',
+        'china' => 'China',
+        'vietname' => 'VietNam',
+        'usa' => 'USA',
+        'japan' => 'Japan',
+        'italy' => 'Italy',
+    ],
+];
